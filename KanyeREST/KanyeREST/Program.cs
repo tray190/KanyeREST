698d019337d0e80e5793bd04ac95053cad2aaed4
tray190 <belltracy411@gmail.com>
2022-01-28 20:08:46 -0600

    update

---
+++ KanyeREST/KanyeREST/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+            QuoteGenerator.RonQuote();
+
+        }
+    }
+}
---
+++ KanyeREST/KanyeREST/QuoteGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeREST
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"_____________");
+            Console.WriteLine($"Kanye said: {kanyeQuote}");
+            Console.WriteLine($"_______________");
+
+        }
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+           string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"_____________");
+            Console.WriteLine($"Ron said: {ronQuote}");
+            Console.WriteLine($"_______________");
+        }
+    }
+}
